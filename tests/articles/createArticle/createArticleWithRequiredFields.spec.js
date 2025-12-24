059186065e380a855d52fcdb30019923a7aae19d
Alex <litvinovalex05@gmail.com>
2025-12-24 22:38:21 +0100

    all done

--- tests/articles/createArticle/createArticleWithRequiredFields.spec.js
+++ tests/articles/createArticle/createArticleWithRequiredFields.spec.js
@@ -2,26 +2,68 @@ import { test } from '../../_fixtures/fixtures';
 import { generateNewArticleData } from '../../../src/common/testData/generateNewArticleData';
 import { signUpUser } from '../../../src/ui/actions/auth/signUpUser';
 
-let article;
+const tagSets = [
+  ['tag1'],
+  ['tag1', 'tag2'],
+  ['tag1', 'tag2', 'tag3', 'tag4', 'tag5'],
+];
 
-test.beforeEach(async ({ page, user, logger }) => {
-  article = generateNewArticleData(logger);
+tagSets.forEach(tags => {
+  test.describe(`Create and edit article with ${tags.length} tag(s)`, () => {
+    let article;
 
-  await signUpUser(page, user);
-});
+    test.beforeEach(async ({ page, user, logger }) => {
+      article = generateNewArticleData(logger);
+      await signUpUser(page, user);
+    });
 
-test('Creat an article with required fields', async ({
-  homePage,
-  createArticlePage,
-  viewArticlePage,
-}) => {
-  await homePage.clickNewArticleLink();
+    test(`User can create an article with ${tags.length} tag(s)`, async ({
+      homePage,
+      createArticlePage,
+      viewArticlePage,
+    }) => {
+      await homePage.clickNewArticleLink();
 
-  await createArticlePage.fillTitleField(article.title);
-  await createArticlePage.fillDescriptionField(article.description);
-  await createArticlePage.fillTextField(article.text);
-  await createArticlePage.clickPublishArticleButton();
+      await createArticlePage.fillTitleField(article.title);
+      await createArticlePage.fillDescriptionField(article.description);
+      await createArticlePage.fillTextField(article.text);
 
-  await viewArticlePage.assertArticleTitleIsVisible(article.title);
-  await viewArticlePage.assertArticleTextIsVisible(article.text);
+      for (const tag of tags) {
+        await createArticlePage.fillTagsField(tag);
+        await createArticlePage.pressEnterToAddTag();
+      }
+
+      await createArticlePage.clickPublishArticleButton();
+
+      await viewArticlePage.assertArticleTitleIsVisible(article.title);
+      await viewArticlePage.assertArticleTextIsVisible(article.text);
+      await viewArticlePage.assertTagsAreVisible(tags);
+    });
+
+    test(`User can remove all tags from an article with
+       ${tags.length} tag(s)`, async ({
+      homePage,
+      createArticlePage,
+      viewArticlePage,
+      editArticlePage,
+    }) => {
+      await homePage.clickNewArticleLink();
+      await createArticlePage.fillTitleField(article.title);
+      await createArticlePage.fillDescriptionField(article.description);
+      await createArticlePage.fillTextField(article.text);
+
+      for (const tag of tags) {
+        await createArticlePage.fillTagsField(tag);
+        await createArticlePage.pressEnterToAddTag();
+      }
+
+      await createArticlePage.clickPublishArticleButton();
+
+      await viewArticlePage.clickEditArticleButton();
+      await editArticlePage.removeAllTags();
+      await editArticlePage.clickPublishArticleButton();
+
+      await viewArticlePage.assertTagsAreNotVisible();
+    });
+  });
 });
--- tests/signUp/signUpNegative.spec.js
+++ tests/signUp/signUpNegative.spec.js
@@ -1,35 +1,62 @@
 import { test } from '../_fixtures/fixtures';
 import {
   EMPTY_USERNAME_MESSAGE,
-  INVALID_EMAIL_MESSAGE,
+  EMPTY_EMAIL_MESSAGE,
   EMPTY_PASSWORD_MESSAGE,
+  INVALID_EMAIL_MESSAGE,
+  PASSWORD_TOO_SHORT_MESSAGE,
 } from '../../src/ui/constants/authErrorMessages';
+import { generateNewUserData } from '../../src/common/testData/generateNewUserData';
 
-test.describe('Sign up negative tests', () => {
-  test('Sign up with empty username', async ({ user, signUpPage }) => {
-    await signUpPage.open();
-    await signUpPage.fillEmailField(user.email);
-    await signUpPage.fillPasswordField(user.password);
-    await signUpPage.clickSignUpButton();
+const user = generateNewUserData();
 
-    await signUpPage.assertErrorMessageContainsText(EMPTY_USERNAME_MESSAGE);
-  });
+const testParameters = [
+  {
+    username: '',
+    email: user.email,
+    password: user.password,
+    message: EMPTY_USERNAME_MESSAGE,
+    title: 'empty username',
+  },
+  {
+    username: user.username,
+    email: '',
+    password: user.password,
+    message: EMPTY_EMAIL_MESSAGE,
+    title: 'empty email',
+  },
+  {
+    username: user.username,
+    email: 'invalidemail',
+    password: user.password,
+    message: INVALID_EMAIL_MESSAGE,
+    title: 'invalid email',
+  },
+  {
+    username: user.username,
+    email: user.email,
+    password: '',
+    message: EMPTY_PASSWORD_MESSAGE,
+    title: 'empty password',
+  },
+  {
+    username: user.username,
+    email: user.email,
+    password: '123',
+    message: PASSWORD_TOO_SHORT_MESSAGE,
+    title: 'password too short',
+  },
+];
 
-  test('Sign up with empty email', async ({ user, signUpPage }) => {
-    await signUpPage.open();
-    await signUpPage.fillUsernameField(user.username);
-    await signUpPage.fillPasswordField(user.password);
-    await signUpPage.clickSignUpButton();
-
-    await signUpPage.assertErrorMessageContainsText(INVALID_EMAIL_MESSAGE);
-  });
-
-  test('Sign up with empty password', async ({ user, signUpPage }) => {
-    await signUpPage.open();
-    await signUpPage.fillUsernameField(user.username);
-    await signUpPage.fillEmailField(user.email);
-    await signUpPage.clickSignUpButton();
-
-    await signUpPage.assertErrorMessageContainsText(EMPTY_PASSWORD_MESSAGE);
+testParameters.forEach(({ username, email, password, message, title }) => {
+  test.describe('Sign up negative tests', () => {
+    test(`Sign up with ${title}`, async ({ signUpPage }) => {
+      await signUpPage.open();
+      await signUpPage.fillUsernameField(username);
+      await signUpPage.fillEmailField(email);
+      await signUpPage.fillPasswordField(password);
+      await signUpPage.clickSignUpButton();
+      await signUpPage.assertErrorMessageContainsText(message);
+    });
   });
 });
